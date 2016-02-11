5d6fee95896aae056bf8a508b7a8924bb2c827b8
saikrish <saikrishnam.nadimpalli@gmail.com>
2016-02-10 19:00:36 -0800

    got it already, new way check
    taking the mongoURI, storing it in a variable and then assigning it to
    mongoose.connect(var).

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ var router = express.Router();
 // DATABASE CONNECTION
 var mongoose = require('mongoose');
 //var localhost = 'mongodb://localhost/names';
-//var mongoURI = 'mongodb://heroku_9nzlstmm:9u9ltiisdagrjntseqgd23u4nu@ds061405.mongolab.com:61405/heroku_9nzlstmm' ;
+var mongoURI = 'mongodb://heroku_9nzlstmm:9u9ltiisdagrjntseqgd23u4nu@ds061405.mongolab.com:61405/heroku_9nzlstmm' ;
 
-mongoose.connect(process.env.MONGOLAB_URI);
+//mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(mongoURI);
+
 
 var db = mongoose.connection ;
 db.on('error',console.error.bind(console,'connection error: '));
